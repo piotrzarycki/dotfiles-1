7cb9ee32a71238701748b227fbfa1956af8886b5
Piotr Zarycki <piotr.zarycki@gmail.com>
2022-07-19 06:44:50 +0200

    next configs

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,18 +1,28 @@
-local utils = require("utils")
-local nmap = utils.nmap
-local imap = utils.imap
-local cmd = vim.cmd
-local api = vim.api
-local fn = vim.fn
-local lsp = vim.lsp
 local lsp_installer = require("nvim-lsp-installer")
-local nvim_lsp = require("lspconfig")
+local lspconfig = require("lspconfig")
 local theme = require("theme")
 local colors = theme.colors
 local icons = theme.icons
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local group = vim.api.nvim_create_augroup("LspConfig", {clear = true})
 
-cmd("autocmd ColorScheme * highlight NormalFloat guibg=" .. colors.bg)
-cmd("autocmd ColorScheme * highlight FloatBorder guifg=white guibg=" .. colors.bg)
+vim.api.nvim_create_autocmd(
+  "ColorScheme",
+  {
+    pattern = "*",
+    command = "highlight NormalFloat guibg=" .. colors.bg,
+    group = group
+  }
+)
+
+vim.api.nvim_create_autocmd(
+  "ColorScheme",
+  {
+    pattern = "*",
+    command = "highlight FloatBorder guifg=white guibg=" .. colors.bg,
+    group = group
+  }
+)
 
 local border = {
   {"🭽", "FloatBorder"},
@@ -29,89 +39,91 @@ local format_async = function(err, _, result, _, bufnr)
   if err ~= nil or result == nil then
     return
   end
-  if not api.nvim_buf_get_option(bufnr, "modified") then
-    local view = fn.winsaveview()
-    lsp.util.apply_text_edits(result, bufnr)
-    fn.winrestview(view)
-    if bufnr == api.nvim_get_current_buf() then
-      api.nvim_command("noautocmd :update")
+  if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+    local view = vim.fn.winsaveview()
+    vim.lsp.util.apply_text_edits(result, bufnr)
+    vim.fn.winrestview(view)
+    if bufnr == vim.api.nvim_get_current_buf() then
+      vim.api.nvim_command("noautocmd :update")
     end
   end
 end
 
-lsp.handlers["textDocument/formatting"] = format_async
+vim.lsp.handlers["textDocument/formatting"] = format_async
 
--- _G makes this function available to vimscript lua calls
-_G.lsp_organize_imports = function()
+local lsp_organize_imports = function()
   local params = {
     command = "_typescript.organizeImports",
-    arguments = {api.nvim_buf_get_name(0)},
+    arguments = {vim.api.nvim_buf_get_name(0)},
     title = ""
   }
-  lsp.buf.execute_command(params)
+  vim.lsp.buf.execute_command(params)
 end
+-- _G makes this function available to vimscript lua calls
+_G.lsp_organize_imports = lsp_organize_imports
 
 -- show diagnostic line with custom border and styling
-_G.lsp_show_diagnostics = function()
+local lsp_show_diagnostics = function()
   vim.diagnostic.open_float({border = border})
 end
 
 local on_attach = function(client, bufnr)
-  cmd [[command! LspDef lua vim.lsp.buf.definition()]]
-  cmd [[command! LspFormatting lua vim.lsp.buf.formatting()]]
-  cmd [[command! LspCodeAction lua vim.lsp.buf.code_action()]]
-  cmd [[command! LspHover lua vim.lsp.buf.hover()]]
-  cmd [[command! LspRename lua vim.lsp.buf.rename()]]
-  cmd [[command! LspOrganize lua lsp_organize_imports()]]
-  cmd [[command! OR lua lsp_organize_imports()]]
-  cmd [[command! LspRefs lua vim.lsp.buf.references()]]
-  cmd [[command! LspTypeDef lua vim.lsp.buf.type_definition()]]
-  cmd [[command! LspImplementation lua vim.lsp.buf.implementation()]]
-  cmd [[command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()]]
-  cmd [[command! LspDiagNext lua vim.lsp.diagnostic.goto_next()]]
-  cmd [[command! LspDiagLine lua lsp_show_diagnostics()]]
-  cmd [[command! LspSignatureHelp lua vim.lsp.buf.signature_help()]]
-
+  vim.cmd [[command! OR lua lsp_organize_imports()]]
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = border})
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.hover, {border = border})
 
-  nmap("gd", ":LspDef<CR>", {bufnr = bufnr})
-  nmap("gr", ":LspRename<CR>", {bufnr = bufnr})
-  nmap("gR", ":LspRefs<CR>", {bufnr = bufnr})
-  nmap("gy", ":LspTypeDef<CR>", {bufnr = bufnr})
-  nmap("K", ":LspHover<CR>", {bufnr = bufnr})
-  nmap("gs", ":LspOrganize<CR>", {bufnr = bufnr})
-  nmap("[a", ":LspDiagPrev<CR>", {bufnr = bufnr})
-  nmap("]a", ":LspDiagNext<CR>", {bufnr = bufnr})
-  nmap("ga", ":LspCodeAction<CR>", {bufnr = bufnr})
-  nmap("<Leader>a", ":LspDiagLine<CR>", {bufnr = bufnr})
-  imap("<C-x><C-x>", "<cmd> LspSignatureHelp<CR>", {bufnr = bufnr})
+  local opts = {noremap = true, silent = true}
+  vim.keymap.set("n", "<leader>aa", lsp_show_diagnostics, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>aq", vim.diagnostic.setloclist, opts)
 
-  if client.resolved_capabilities.document_highlight then
-    api.nvim_exec(
-      [[
-    augroup lsp_document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-    ]],
-      false
+  local bufopts = {noremap = true, silent = true, buffer = bufnr}
+  vim.keymap.set("n", "gO", lsp_organize_imports, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "gR", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<C-x><C-x>", vim.lsp.buf.signature_help, bufopts)
+
+  if client.server_capabilities.document_highlight then
+    vim.api.nvim_create_autocmd(
+      "CursorHold",
+      {
+        pattern = "*",
+        callback = function()
+          vim.lsp.buf.document_highlight()
+        end,
+        group = group
+      }
+    )
+    vim.api.nvim_create_autocmd(
+      "CursorMoved",
+      {
+        pattern = "*",
+        callback = function()
+          vim.lsp.buf.clear_references()
+        end,
+        group = group
+      }
     )
   end
 
   -- disable document formatting (currently handled by formatter.nvim)
-  client.resolved_capabilities.document_formatting = false
+  client.server_capabilities.document_formatting = false
 
-  if client.resolved_capabilities.document_formatting then
-    api.nvim_exec(
-      [[
-        augroup LspAutocommands
-        autocmd! * <buffer>
-        autocmd BufWritePost <buffer> LspFormatting
-        augroup END
-      ]],
-      true
+  if client.server_capabilities.document_formatting then
+    vim.api.nvim_create_autocmd(
+      "BufEnter",
+      {
+        pattern = "*",
+        callback = function()
+          vim.lsp.buf.formatting()
+        end,
+        group = group
+      }
     )
   end
 end
@@ -157,15 +169,18 @@ local lua_settings = {
     workspace = {
       -- Make the server aware of Neovim runtime files
       library = {
-        [fn.expand("$VIMRUNTIME/lua")] = true,
-        [fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
       }
     }
   }
 }
 
-local function make_config()
-  local capabilities = lsp.protocol.make_client_capabilities()
+local function make_config(callback)
+  callback = callback or function(config)
+      return config
+    end
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = {
@@ -175,61 +190,105 @@ local function make_config()
     }
   }
   capabilities.textDocument.colorProvider = {dynamicRegistration = false}
+  capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
-  return {
-    capabilities = capabilities,
-    on_attach = on_attach
-  }
+  return callback(
+    {
+      capabilities = capabilities,
+      on_attach = on_attach
+    }
+  )
 end
 
-lsp_installer.on_server_ready(
-  function(server)
-    local opts = make_config()
-    if server.name == "lua" then
-      opts.settings = lua_settings
-      opts.root_dir = function(fname)
-        local util = require("lspconfig/util")
-        return util.find_git_ancestor(fname) or util.path.dirname(fname)
-      end
-    elseif server.name == "vim" then
-      opts.init_options = {isNeovim = true}
-    elseif server.name == "diagnosticls" then
-      opts = diagnosticls_settings
-    elseif server.name == "tsserver" then
-      local capabilities = opts.capabilities
-      opts.capabiltiies = require("cmp_nvim_lsp").update_capabilities(capabilities)
-      opts.root_dir = nvim_lsp.util.root_pattern("package.json")
-      opts.handlers = {
-        ["textDocument/definition"] = function(err, result, ctx, config)
+lsp_installer.setup(
+  {
+    ensure_installed = {
+      "eslint",
+      "tsserver",
+      "sumneko_lua",
+      "denols",
+      "vimls"
+    },
+    automatic_installation = true,
+    ui = {
+      check_outdated_servers_on_open = true
+    }
+  }
+)
+
+lspconfig.tsserver.setup(
+  make_config(
+    function(config)
+      config.root_dir = lspconfig.util.root_pattern("tsconfig.json")
+      config.handlers = {
+        ["textDocument/definition"] = function(err, result, ctx, conf)
           -- if there is more than one result, just use the first one
           if #result > 1 then
             result = {result[1]}
           end
-          vim.lsp.handlers["textDocument/definition"](err, result, ctx, config)
+          vim.lsp.handlers["textDocument/definition"](err, result, ctx, conf)
         end
       }
-    elseif server.name == "denols" then
-      opts.root_dir = nvim_lsp.util.root_pattern("deno.json")
-      opts.init_options = {
+      return config
+    end
+  )
+)
+
+lspconfig.denols.setup(
+  make_config(
+    function(config)
+      config.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+      config.init_options = {
         lint = true
       }
+      return config
     end
+  )
+)
 
-    server:setup(opts)
-  end
+lspconfig.sumneko_lua.setup(
+  make_config(
+    function(config)
+      config.settings = lua_settings
+      config.root_dir = function(fname)
+        local util = require("lspconfig/util")
+        return util.find_git_ancestor(fname) or util.path.dirname(fname)
+      end
+      config.root_dir = lspconfig.util.root_pattern("lua.json")
+      return config
+    end
+  )
+)
+
+lspconfig.vimls.setup(
+  make_config(
+    function(config)
+      config.init_options = {isNeovim = true}
+      return config
+    end
+  )
+)
+
+lspconfig.diagnosticls.setup(
+  make_config(
+    function(config)
+      config.settings = diagnosticls_settings
+      return config
+    end
+  )
 )
 
 -- set up custom symbols for LSP errors
 local signs = {Error = icons.error, Warning = icons.warning, Warn = icons.warning, Hint = icons.hint, Info = icons.hint}
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, {text = icon, texthl = hl})
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
 end
 
 -- Set colors for completion items
-cmd("highlight! CmpItemAbbrMatch guibg=NONE guifg=" .. colors.lightblue)
-cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=" .. colors.lightblue)
-cmd("highlight! CmpItemKindFunction guibg=NONE guifg=" .. colors.magenta)
-cmd("highlight! CmpItemKindMethod guibg=NONE guifg=" .. colors.magenta)
-cmd("highlight! CmpItemKindVariable guibg=NONE guifg=" .. colors.blue)
-cmd("highlight! CmpItemKindKeyword guibg=NONE guifg=" .. colors.fg)
+vim.cmd("highlight! CmpItemAbbrMatch guibg=NONE guifg=" .. colors.lightblue)
+vim.cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=" .. colors.lightblue)
+vim.cmd("highlight! CmpItemKindFunction guibg=NONE guifg=" .. colors.magenta)
+vim.cmd("highlight! CmpItemKindMethod guibg=NONE guifg=" .. colors.magenta)
+vim.cmd("highlight! CmpItemKindVariable guibg=NONE guifg=" .. colors.blue)
+vim.cmd("highlight! CmpItemKindKeyword guibg=NONE guifg=" .. colors.fg)
--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -5,38 +5,8 @@ local icons = require("theme").icons
 local view = require("nvim-tree.view")
 _G.NvimTreeConfig = {}
 
-vim.g.nvim_tree_icons = {
-  default = icons.file,
-  symlink = icons.symlink,
-  git = {
-    unstaged = icons.unmerged,
-    staged = icons.staged,
-    unmerged = icons.unmerged,
-    renamed = icons.renamed,
-    untracked = icons.untracked,
-    deleted = icons.deleted,
-    ignored = icons.ignored
-  },
-  folder = {
-    arrow_open = icons.arrow_open,
-    arrow_closed = icons.arrow_closed,
-    default = icons.default,
-    open = icons.open,
-    empty = icons.empty,
-    empty_open = icons.empty_open,
-    symlink = icons.symlink,
-    symlink_open = icons.symlink_open
-  },
-  lsp = {
-    hint = icons.hint,
-    info = icons.info,
-    warning = icons.warning,
-    error = icons.error
-  }
-}
-
 function NvimTreeConfig.find_toggle()
-  if view.win_open() then
+  if view.is_visible() then
     view.close()
   else
     vim.cmd("NvimTreeToggle")
@@ -45,14 +15,51 @@ end
 
 nnoremap("<leader>k", "<CMD>lua NvimTreeConfig.find_toggle()<CR>")
 
-view.width = 40
-
 nvimtree.setup {
   disable_netrw = false,
   hijack_netrw = true,
-  auto_close = false,
   diagnostics = {
-    enable = false
+    enable = false,
+    icons = {
+      hint = icons.hint,
+      info = icons.info,
+      warning = icons.warning,
+      error = icons.error
+    }
+  },
+  renderer = {
+    add_trailing = false,
+    group_empty = true,
+    highlight_git = true,
+    highlight_opened_files = "all",
+    indent_markers = {
+      enable = true
+    },
+    icons = {
+      glyphs = {
+        default = icons.file,
+        symlink = icons.symlink,
+        git = {
+          unstaged = icons.unmerged,
+          staged = icons.staged,
+          unmerged = icons.unmerged,
+          renamed = icons.renamed,
+          untracked = icons.untracked,
+          deleted = icons.deleted,
+          ignored = icons.ignored
+        },
+        folder = {
+          arrow_open = icons.arrow_open,
+          arrow_closed = icons.arrow_closed,
+          default = icons.default,
+          open = icons.open,
+          empty = icons.empty,
+          empty_open = icons.empty_open,
+          symlink = icons.symlink,
+          symlink_open = icons.symlink_open
+        }
+      }
+    }
   },
   update_focused_file = {
     enable = true,
@@ -60,12 +67,10 @@ nvimtree.setup {
   },
   git = {
     enable = true,
-    ignore = true,
-    timeout = 500,
-  },  
-    view = {
-    width = 40,
-    side = "left",
-    auto_resize = true
+    ignore = false
+  },
+  view = {
+    width = 60,
+    side = "left"
   }
 }
